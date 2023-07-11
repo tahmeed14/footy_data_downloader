fe82d7d36a554c769f6637940f4e2100b8b0afe9
Tahmeed Tureen <tureen@umich.edu>
2023-07-11 00:09:12 -0400

    processed standard and possession stats

--- 1_helper_functions/fbref_helpers.R
+++ 1_helper_functions/fbref_helpers.R
@@ -1,5 +1,104 @@
 # Author: Tahmeed Tureen <tureen@umich.edu>
-# Helper functions that leverage worldfootballR to download data from FBref.com
+# Helper functions that leverage worldfootballR to download and process data from FBref.com
 
-require(worldfootballR) # Credit to Jason Zivkovic: https://jaseziv.github.io/worldfootballR/index.html
+library(worldfootballR) # Credit to Jason Zivkovic: https://jaseziv.github.io/worldfootballR/index.html
+library(dplyr)
+library(corrplot)
+
+# Function to visualize the correlation plots in hierarchical cluster order for LVA
+vizCorrelation <- function(data_in) {
+  require(dplyr)
+  require(corrplot)
+  
+  cat("No. of Players:", nrow(data_in), "\n")
+  # Drop non-numeric variables
+  function_df <- data_in %>% 
+    dplyr::select_if(is.numeric)
+  
+  # Create correlation matrix & visualize
+  corrplot::corrplot(corr = cor(function_df),
+                     method = "square",
+                     order = "hclust",
+                     tl.col = 'black',
+                     tl.cex = 0.75)
+}
+
+# Function to visualize distributions of all continuous variables in data_in
+vizContinuous <- function(data_in) {
+  
+  vars_plot <- colnames(data_in %>% select_if(is.numeric))
+  
+  par(mfrow = c(3,3))
+  for (var in vars_plot) {
+    hist(data_in[,var], xlab = var, main = " ")
+    abline(v = mean(data_in[,var]), col = "red", lwd = 3)
+  }
+}
+
+# processStandard: Function to process and subset standard data for LVA
+processStandard <- function(data_in, season_in) {
+  require(dplyr)
+  n_prev <- nrow(data_in)
+  
+  output_data <- data_in %>% 
+    filter(Season == season_in) %>% 
+    filter(!is.na(Min_Playing_Time)) %>%
+    mutate(MP = if_else(is.na(MP),
+                        true = 0,
+                        false = MP),
+           MP_Playing_Time = if_else(is.na(MP_Playing_Time),
+                                     true = 0,
+                                     false = MP_Playing_Time)) %>% # convert the NAs to 0's
+    mutate(MP_Playing_Time = MP + MP_Playing_Time) %>% # finally, combine the two repetitive variables into one
+    select(-c(MP, 
+              PlayerURL,
+              CrdR,
+              CrdY,
+              xG_Expected,
+              npxG_Expected,
+              xAG_Expected,
+              `npxG+xAG_Expected`,
+              `xG_Per_Minutes`,
+              npxG_Per_Minutes,
+              `npxG+xAG_Per_Minutes`,
+              xAG_Expected,
+              xAG_Per_Minutes,
+              `xG+xAG_Per_Minutes`)) %>% 
+    mutate(Age = as.numeric(Age))
+  
+  cat("League(s):", unique(output_data$Comp), "\n")
+  cat("Season(s):", unique(output_data$Season), "\n")
+  cat("No. of players dropped due to zero playing minutes:", 
+      n_prev - nrow(output_data) , "from", n_prev, "players \n")
+  
+  return(output_data)
+}
+
+# processPossession: Function to process and subset standard data for LVA
+processPossession <- function(main_data_in, data_in, season_in){
+  require(dplyr)
+  
+  append_data <- data_in %>% 
+    filter(Season == season_in) %>%
+    filter(Mins_Per_90 != 0) %>% 
+    select(-c(Age,
+              Nation,
+              Pos,
+              Mins_Per_90,
+              PlayerURL)) %>% 
+    mutate(Succ_percent_Take_Ons = if_else(is.na(Succ_percent_Take_Ons),
+                                           true = 0,
+                                           false = Succ_percent_Take_Ons),
+           Tkld_percent_Take_Ons = if_else(is.na(Tkld_percent_Take_Ons),
+                                           true = 0,
+                                           false = Tkld_percent_Take_Ons))
+  
+  vizCorrelation(data_in = append_data)
+  vizContinuous(data_in = append_data)
+  
+  output_data <- main_data_in %>% 
+    left_join(append_data, by = c("Season", "Player", "Squad", "Comp"))
+  
+  return(output_data)
+}
 
--- 2_data_processing/fbref_player_stats.R
+++ 2_data_processing/fbref_player_stats.R
@@ -1,130 +1,44 @@
 # Author: Tahmeed Tureen <tureen@umich.edu>
-# Description: Code script that leverages the worldfootballR library/package to pull player statistics from FBref.com
 
 library(worldfootballR) # Credit to Jason Zivkovic: https://jaseziv.github.io/worldfootballR/index.html
 library(dplyr)
-library(ggplot2)
 
-seasons <- 2017:2023
+# Process the Standard Stats from FBref.com
 
-pl_seasons_url <- fb_league_urls(country = "ENG", gender = "M", season_end_year = seasons, tier = "1st")
-buli_seasons_url <- fb_league_urls(country = "GER", gender = "M", season_end_year = seasons, tier = "1st")
-laliga_seasons_url <- fb_league_urls(country = "ESP", gender = "M", season_end_year = seasons, tier = "1st")
-seriea_seasons_url <- fb_league_urls(country = "ITA", gender = "M", season_end_year = seasons, tier = "1st")
+standard_data <- readRDS(file = "3_data/player_standard_stats.RDS")
 
-# player_data <- fb_team_player_stats(team_urls= pl_team_urls[1], stat_type= 'standard')
-
-stat_types <- c("standard", "shooting", "passing", "passing_types",
-                "gca", "defense", "possession", "playing_time",
-                "misc", "keeper", "keeper_adv")
-
-
-pl_team_urls <- fb_teams_urls(pl_seasons_url[2])
-league_stats <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                                     stat_type= "possession",
-                                                     time_pause = 5)
-
-pl_team_urls3 <- fb_teams_urls(pl_seasons_url[3])
-league_stats3 <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls3, 
-                                                     stat_type= "possession",
-                                                     time_pause = 5)
-
-pl_team_urls4 <- fb_teams_urls(pl_seasons_url[4])
-league_stats4 <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls4, 
-                                                      stat_type= "possession",
-                                                      time_pause = 5)
-
-pl_team_urls5 <- fb_teams_urls(pl_seasons_url[5])
-league_stats5 <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls5, 
-                                                      stat_type= "possession",
-                                                      time_pause = 5)
-
-pl_team_urls6 <- fb_teams_urls(pl_seasons_url[6])
-league_stats6 <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls6, 
-                                                      stat_type= "possession",
-                                                      time_pause = 5)
-
-possession_data <- testdata %>% 
-  bind_rows(league_stats) %>%
-  bind_rows(league_stats3) %>%
-  bind_rows(league_stats4) %>%
-  bind_rows(league_stats5) %>%
-  bind_rows(league_stats6)
+main_data <- standard_data %>% 
+  filter(!is.na(Min_Playing_Time)) %>% # 739 players to 569 players with non-zero playing time
+  mutate(MP = if_else(is.na(MP),
+                      true = 0,
+                      false = MP),
+         MP_Playing_Time = if_else(is.na(MP_Playing_Time),
+                                   true = 0,
+                                   false = MP_Playing_Time)) %>% # convert the NAs to 0's
+  mutate(MP_Playing_Time = MP + MP_Playing_Time) %>% # finally, combine the two repetitive variables into one
+  select(-c(MP, PlayerURL)) %>% 
+  mutate(Age = as.numeric(Age))
   
+std_corr <- cor(main_data %>% 
+                  select(-c(Season, Squad, Comp, Player, Nation, Pos)), 
+                use="complete.obs")
 
-saveRDS(object = possession_data, file = "3_data/player_possession_stats.RDS")
-
-
-pl_team_urls <- fb_teams_urls(pl_seasons_url[1])
-
-# standard
-league_stats_std <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                                         stat_type= "standard",
-                                                         time_pause = 5)
-
-standard_data <- league_stats_std
-saveRDS(object = standard_data, file = "3_data/player_standard_stats.RDS")
+corrplot::corrplot(std_corr,
+                   method = "square",
+                   order = "hclust",
+                   tl.col = 'black',
+                   tl.cex = 0.75)
 
 
-stat_types <- c("standard", "shooting", "passing", "passing_types",
-                "gca", "defense", "possession", "playing_time",
-                "misc", "keeper", "keeper_adv")
+corrplot(cor(dat, use="complete.obs"),
+         method = "square",
+         order = "hclust", # order by hierarchical cluster
+         tl.col='black',
+         tl.cex=.75) 
 
-# shooting
-stat_j <- "shooting"
-shooting_data <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                                     stat_type= stat_j,
-                                                     time_pause = 3)
+# Process the Shooting Data
+shooting_data <- readRDS(file = "3_data/player_shooting_stats.RDS")
 
-saveRDS(object = shooting_data, file = "3_data/player_shooting_stats.RDS")
-
-# passing
-stat_j <- "passing"
-passing_data <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                              stat_type= stat_j,
-                                              time_pause = 3)
-
-saveRDS(object = passing_data, file = "3_data/player_passing_stats.RDS")
-
-# passing_types
-stat_j <- "passing_types"
-passing_types_data <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                              stat_type= stat_j,
-                                              time_pause = 3)
-
-saveRDS(object = passing_types_data, file = "3_data/player_passing_types_stats.RDS")
-
-# gca
-stat_j <- "gca"
-gca_data <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                              stat_type= stat_j,
-                                              time_pause = 3)
-
-saveRDS(object = gca_data, file = "3_data/player_gca_stats.RDS")
-
-# defense
-stat_j <- "defense"
-defense_data <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                              stat_type= stat_j,
-                                              time_pause = 3)
-
-saveRDS(object = defense_data, file = "3_data/player_defense_stats.RDS")
-
-# possession
-stat_j <- "possession"
-possession_data <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                              stat_type= stat_j,
-                                              time_pause = 3)
-
-saveRDS(object = possession_data, file = "3_data/player_possession_stats.RDS")
-
-# misc
-stat_j <- "misc"
-misc_data <- worldfootballR::fb_team_player_stats(team_urls= pl_team_urls, 
-                                                        stat_type= stat_j,
-                                                        time_pause = 3)
-
-saveRDS(object = misc_data, file = "3_data/player_misc_stats.RDS")
-
-
-
+main_data <- shooting_data %>% 
+  select(-c(Nation, Pos, Age, Mins_Per_90)) %>% 
+  right_join(main_data, by = c("Season", "Player", "Squad", "Comp"))
