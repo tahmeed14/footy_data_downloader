b3398028d6e0a6592a9bc36fc09cc9c278f2006f
Tahmeed Tureen <tureen@umich.edu>
2023-07-13 01:42:42 -0400

    processed all seven datasets and compiled into 1 file

--- 1_helper_functions/fbref_helpers.R
+++ 1_helper_functions/fbref_helpers.R
@@ -102,3 +102,178 @@ processPossession <- function(main_data_in, data_in, season_in){
   return(output_data)
 }
 
+# processPass:
+processPass <- function(main_data_in, data_in, season_in) {
+  require(dplyr)
+  
+  append_data <- data_in %>% 
+    filter(Mins_Per_90 != 0) %>% 
+    select(-c(Nation,
+              Pos,
+              Age,
+              Mins_Per_90,
+              PlayerURL)) %>% 
+    mutate(Cmp_percent_Total = if_else(is.na(Cmp_percent_Total),
+                                       true = 0,
+                                       false = Cmp_percent_Total),
+           Cmp_percent_Short = if_else(is.na(Cmp_percent_Short),
+                                       true = 0,
+                                       false = Cmp_percent_Short),
+           Cmp_percent_Medium = if_else(is.na(Cmp_percent_Medium),
+                                        true = 0,
+                                        false = Cmp_percent_Medium),
+           Cmp_percent_Long = if_else(is.na(Cmp_percent_Long),
+                                      true = 0,
+                                      false = Cmp_percent_Long))
+  
+  # Need to impute NAs with Zeros for Percent Variables
+  
+  numeric_vars <- colnames(append_data %>% select_if(is.numeric))
+  
+  append_data <- append_data %>% 
+    rename_with(.cols = numeric_vars, 
+                .fn = function(x) paste0("Pass_", x)) # add a prefix "Pass" to variable names
+  
+  vizCorrelation(data_in = append_data)
+  vizContinuous(data_in = append_data)
+  
+  output_data <- main_data_in %>% 
+    left_join(append_data, by = c("Season", "Player", "Squad", "Comp"))
+  
+  return(output_data)
+}
+
+# processPass:
+processPassTypes <- function(main_data_in, data_in, season_in) {
+  require(dplyr)
+  
+  append_data <- data_in %>% 
+    filter(Mins_Per_90 != 0) %>% 
+    select(-c(Nation,
+              Pos,
+              Age,
+              Mins_Per_90,
+              PlayerURL)) %>% 
+    rename(Pass_Attempts = Att)
+  
+  vizCorrelation(data_in = append_data)
+  vizContinuous(data_in = append_data)
+  
+  output_data <- main_data_in %>% 
+    left_join(append_data, by = c("Season", "Player", "Squad", "Comp"))
+  
+  return(output_data)
+}
+
+# SGCA
+processSGCA <- function(main_data_in, data_in, season_in) {
+  require(dplyr)
+  
+  append_data <- data_in %>% 
+    filter(Mins_Per_90 != 0) %>% 
+    select(-c(Nation,
+              Pos,
+              Age,
+              Mins_Per_90,
+              PlayerURL))
+  
+  vizCorrelation(data_in = append_data)
+  vizContinuous(data_in = append_data)
+  
+  output_data <- main_data_in %>% 
+    left_join(append_data, by = c("Season", "Player", "Squad", "Comp"))
+  
+  return(output_data)
+}
+
+# DEF
+processDef <- function(main_data_in, data_in, season_in) {
+  require(dplyr)
+  
+  append_data <- data_in %>% 
+    filter(Mins_Per_90 != 0) %>% 
+    select(-c(Nation,
+              Pos,
+              Age,
+              Mins_Per_90,
+              PlayerURL)) %>% 
+    mutate(Tkl_percent_Challenges = if_else(is.na(Tkl_percent_Challenges),
+                                            true = 0,
+                                            false = Tkl_percent_Challenges))
+  
+  vizCorrelation(data_in = append_data)
+  vizContinuous(data_in = append_data)
+  
+  output_data <- main_data_in %>% 
+    left_join(append_data, by = c("Season", "Player", "Squad", "Comp"))
+  
+  return(output_data)
+}
+
+# Misc
+processMisc <- function(main_data_in, data_in, season_in) {
+  require(dplyr)
+  
+  append_data <- data_in %>% 
+    filter(Mins_Per_90 != 0) %>% 
+    select(-c(Nation,
+              Pos,
+              Age,
+              Mins_Per_90,
+              PlayerURL,
+              CrdY,
+              CrdR,
+              `2CrdY`)) %>% 
+    mutate(Won_percent_Aerial_Duels = if_else(condition = is.na(Won_percent_Aerial_Duels),
+                                              true = 0,
+                                              false = Won_percent_Aerial_Duels))
+  
+  vizCorrelation(data_in = append_data)
+  vizContinuous(data_in = append_data)
+  
+  output_data <- main_data_in %>% 
+    left_join(append_data, by = c("Season", "Player", "Squad", "Comp"))
+  
+  return(output_data)
+  
+}
+
+# Shooting
+processShots <- function(main_data_in, data_in, season_in) {
+  require(dplyr)
+  
+  append_data <- data_in %>% 
+    filter(Mins_Per_90 != 0) %>% 
+    select(-c(Nation,
+              Pos,
+              Age,
+              Mins_Per_90,
+              PlayerURL)) %>% 
+    mutate(G_per_Sh_Standard = if_else(condition = is.na(G_per_Sh_Standard),
+                                       true = 0,
+                                       false = G_per_Sh_Standard),
+           G_per_SoT_Standard = if_else(condition = is.na(G_per_SoT_Standard),
+                                        true = 0,
+                                        false = G_per_SoT_Standard),
+           Dist_Standard = if_else(condition = is.na(Dist_Standard),
+                                   true = 0,
+                                   false = Dist_Standard),
+           `np:G_minus_xG_Expected` = if_else(condition = is.na(`np:G_minus_xG_Expected`),
+                                              true = 0,
+                                              false = `np:G_minus_xG_Expected`),
+           SoT_percent_Standard = if_else(condition = is.na(SoT_percent_Standard),
+                                   true = 0,
+                                   false = SoT_percent_Standard),
+           npxG_per_Sh_Expected = if_else(condition = is.na(npxG_per_Sh_Expected),
+                                   true = 0,
+                                   false = npxG_per_Sh_Expected))
+  
+  vizCorrelation(data_in = append_data)
+  vizContinuous(data_in = append_data)
+  
+  output_data <- main_data_in %>% 
+    left_join(append_data, by = c("Season", "Player", "Squad", "Comp"))
+  
+  return(output_data)
+  
+}
